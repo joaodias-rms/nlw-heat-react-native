609b24db8fa5486df9233cf372976b263f7b6ad8
joao <jvd.ramos@hotmail.com>
2021-10-25 11:17:16 -0400

    adicionado gradiente no avatar

--- src/components/UserPhoto/index.tsx
+++ src/components/UserPhoto/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import AvatarImg from "../../assets/avatar.png";
 
 import { styles } from "./styles";
+import { COLORS } from "../../theme";
 
 const Sizes = {
   small: {
@@ -26,12 +28,26 @@ export function UserPhoto({ imageUri, sizes = "normal" }: Props) {
   const { containerSize, avatarSize } = Sizes[sizes];
 
   return (
-    <Image
-      source={{ uri: imageUri || Avatar_Default}}
+    <LinearGradient
+      colors={[COLORS.PINK, COLORS.YELLOW]}
+      start={{x: 0, y: 0.8}}
+      end={{x:0.9, y:1}}
       style={[
-        styles.avatar,
-        { width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2 },
+        styles.container,
+        { width: containerSize, height: containerSize, borderRadius: containerSize / 2 },
       ]}
-    />
+    >
+      <Image
+        source={{ uri: imageUri || Avatar_Default }}
+        style={[
+          styles.avatar,
+          {
+            width: avatarSize,
+            height: avatarSize,
+            borderRadius: avatarSize / 2,
+          },
+        ]}
+      />
+    </LinearGradient>
   );
 }
