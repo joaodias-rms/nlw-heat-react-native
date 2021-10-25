d0f6fc5840d307a8f786ff44e6e2b4901b0353ec
joao <jvd.ramos@hotmail.com>
2021-10-25 15:11:13 -0400

    adicionada renderização condicional

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-import { Header } from '../../components/Header'
-import { MessageList } from '../../components/MessageList'
-import { SignInBox } from '../../components/SignInBox'
-import { SendMessageForm } from '../../components/SendMessageForm'
+import { Header } from "../../components/Header";
+import { MessageList } from "../../components/MessageList";
+import { SignInBox } from "../../components/SignInBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
 
-import { styles } from './styles'
+import { styles } from "./styles";
+import { useAuth } from "../../hooks/auth";
 
 export function Home() {
-    return (
-        <View style={styles.container}>
-            <Header />
-            <MessageList />
-            <SendMessageForm/>
-            {/* <SignInBox /> */}
-        </View>
-    )
-}
+  const { user } = useAuth();
 
+  return (
+    <View style={styles.container}>
+      <Header />
+      <MessageList />
+
+      {user ? <SendMessageForm /> : <SignInBox />}
+    </View>
+  );
+}
