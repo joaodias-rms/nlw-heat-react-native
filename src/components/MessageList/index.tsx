0f2b4f47707a96f693daf4a0ff218c07c941d0ce
joao <jvd.ramos@hotmail.com>
2021-10-25 16:10:50 -0400

    criado listagem de mensagens

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import {
-  ScrollView
-} from 'react-native';
+import { ScrollView } from "react-native";
+import { api } from "../../services/api";
+import { io } from "socket.io-client";
+import { MESSAGES_EXAMPLE } from "../../utils/messages";
+import { Message, MessageProps } from "../Message";
 
-import { Message } from '../Message';
+import { styles } from "./styles";
 
-import { styles } from './styles';
+let messageQueue: MessageProps[] = MESSAGES_EXAMPLE;
 
-const message = {
-    id: '1',
-    text: 'Tester',
-    user: {
-      name: 'Juão',
-      avatar_url: 'https://github.com/joaodias-rms.png',
+const socket = io(String(api.defaults.baseURL));
+socket.on("new_message", (new_message) => {
+  messageQueue.push(new_message);
+  console.log(new_message);
+});
+
+export function MessageList() {
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessage() {
+      const message = await api.get("/message/last3");
+      setCurrentMessages(message.data);
     }
-}
+    fetchMessage();
+  }, []);
 
-export function MessageList(){
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messageQueue.length > 0) {
+        setCurrentMessages((prevState) => [
+          messageQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messageQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer)
+  }, []);
+
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.content} keyboardShouldPersistTaps='never'>
-        <Message data={message}/>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+    >
+      {currentMessages.map((message) => (
+        <Message key={message.id} data={message} />
+      ))}
     </ScrollView>
   );
 }
