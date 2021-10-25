1da32ac755427856076a1e94a39aec9c9b51d848
joao <jvd.ramos@hotmail.com>
2021-10-25 16:11:03 -0400

    adicionado envio de mensagem para o backend

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 
-import { View, TextInput } from "react-native";
+import { View, TextInput, Alert, Keyboard } from "react-native";
 
 import { Button } from "../Button";
 
 import { COLORS } from "../../theme";
 import { styles } from "./styles";
+import { api } from "../../services/api";
 
 export function SendMessageForm() {
   const [message, setMessage] = useState("");
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleMessageSubmit() {
+    const messageFormatted = message.trim();
+
+    if (messageFormatted.length > 0) {
+      setSendingMessage(true)
+      await api.post("/message", { message: messageFormatted });
+      setMessage("");
+      Keyboard.dismiss();
+      setSendingMessage(false);
+      Alert.alert("Mensagem enviada com sucesso");
+    } else {
+      Alert.alert("Escreva a mensagem");
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -29,6 +45,8 @@ export function SendMessageForm() {
         title="Enviar Mensagem"
         color={COLORS.WHITE}
         backgroundColor={COLORS.PINK}
+        isLoading={sendingMessage}
+        onPress={handleMessageSubmit}
       />
     </View>
   );
